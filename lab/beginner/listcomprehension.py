22dc092ecbbcca48603c24114a30d6272f770f21
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-08 23:05:29 +0200

    list comprehension

---
+++ lab/beginner/listcomprehension.py
@@ -0,0 +1,39 @@
+# filtering even numbers
+# numbers=[1,2,3,4,5,6,7,8,9,10]
+# even_numbers=[num for num in numbers  if num%2==0]
+# print(even_numbers)
+
+# transformations 
+# square each number in a list
+# numbers=[1,2,4,5,6,7,8,9,10]
+# sq_numbers=[num**2 for num in numbers]
+# print(sq_numbers)
+
+# generation
+# first 10 squared numbers
+# sq=[num**2 for num in range(1,11)]
+# print(sq)
+
+# combining filtering and transformation
+# even_sq=[num**2 for num in range(1,11) if num%2==0]
+# print(even_sq)
+
+# nested list comprehension
+# generating a pair 
+# pairs=[(i,j) for i in range(1,4) for j in range(1,4)]
+# print(pairs)
+
+# flattening a nested list
+# nested_list=[[1,2,3],[4,5,6],[7,8,9]]
+# oned=[num for sublist in nested_list for num in sublist]
+# print(oned)
+
+# conditional assignment
+# numbers=[1,2,3,-1,-2,-3]
+# nonzero=[num if num>0 else 0 for num in numbers]
+# print(nonzero)
+
+# generating a list of strings
+# names=["mugisha","pascal","daddy"]
+# listofstrings=[f"hello {name}" for name in names]
+# print(listofstrings)
