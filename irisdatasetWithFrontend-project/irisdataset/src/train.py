a0600de92d1380bc76e626591e0d5927a0122aa3
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-18 21:00:53 +0200

    training the model

--- irisdatasetWithFrontend-project/irisdataset/src/train.py
+++ irisdatasetWithFrontend-project/irisdataset/src/train.py
@@ -12,6 +12,7 @@ x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42
 
 model=RandomForestClassifier(n_estimators=100,random_state=42)
 model.fit(x_train,y_train)
+joblib.dump(model,"../model/iris.joblib")
 y_pred=model.predict(x_test)
 accuracy=accuracy_score(y_test,y_pred)
 print(f"the accuracy is {accuracy}")
