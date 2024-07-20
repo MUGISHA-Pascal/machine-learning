193604f51039cc9a2d71f3fbd7cf16e5f4442004
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-20 19:54:40 +0200

    model intergration

---
+++ irisdatasetWithFrontend-project/app.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,jsonify
+import joblib
+
+app=Flask(__name__)
+@app.route("/predict",method=["POST"])
+def predict():
+    data=request.json
+    features=data["features"]
+    model=joblib.load("./iris-dataset")
+    prediction=model.predict([features])
+    return jsonify({"prediction":prediction.toList()})
+
+if __name__ == "__main__":
+    app.run(port=5000,debug=True)
