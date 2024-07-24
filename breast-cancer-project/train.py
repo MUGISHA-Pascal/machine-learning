f3bc12dd938b83cb07333e380858f601ad0879b2
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-24 23:27:14 +0200

    analysis graph

--- breast-cancer-project/train.py
+++ breast-cancer-project/train.py
@@ -3,6 +3,8 @@ from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import mean_squared_error,explained_variance_score
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+import joblib
+import matplotlib.pyplot as plt
 
 data_bunch = load_breast_cancer()
 data = pd.DataFrame(data_bunch.data, columns=data_bunch.feature_names)
@@ -12,8 +14,16 @@ y=data["worst fractal dimension"]
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
 model=LinearRegression()
 model.fit(x_train,y_train)
+joblib.dump(model,"./model/cancer.joblib")
 y_pred=model.predict(x_test)
 meansq=mean_squared_error(y_test,y_pred)
 print(f"the mean square error is : {meansq}")
 variance_score=explained_variance_score(y_test,y_pred)
 print(f"the variance score is : {variance_score}")
+
+plt.scatter(y_test,y_pred)
+plt.xlabel("x features")
+plt.ylabel("y features")
+plt.title("the analysis")
+plt.show()
+
