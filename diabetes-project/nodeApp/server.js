e8214059951907c1aeed5ccbcabd534b77f711a8
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-26 14:49:42 +0200

    diabetes project

---
+++ diabetes-project/nodeApp/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const axios = require("axios");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(bodyparser.json());
+app.use(express.static("public"));
+
+app.post("/prediction", async (req, res) => {
+  try {
+    const response = await axios.post("http://localhost:5000/predict", {
+      features,
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app running on http://localhost:3000");
+});
