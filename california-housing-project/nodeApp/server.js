10a8ef9f788f00363f86f6584ed1115487f8a613
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-07-29 22:30:53 +0200

    model intergration

---
+++ california-housing-project/nodeApp/server.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.post("/prediction", async (req, res) => {
+  try {
+    const response = await axios.post("http://localhost:5000/prediction", {
+      features: req.body.features,
+    });
+    res.json(response.data);
+    console.log(response.data.prediction);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app is running on http://localhost:3000");
+});
